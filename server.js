6e5eb47e4bc4ef72d665d43fe3695297a1e60eaf
fliptheweb <itisshortemailaddress@gmail.com>
2011-08-26 03:46:46 +1100

    Move server to nodejs module

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 var http = require("http");
 
-http.createServer(function(request, response) {
-	response.writeHead(200, {"Content-Type": "text/html"});
-	response.write("Vladivostok Developer Conference #0");
-	response.end();
-}).listen(8888);
+/**
+ * Start out http server
+ */
+function start(){
+	http.createServer(function(request, response) {
+		response.writeHead(200, {"Content-Type": "text/html"});
+		response.write("Vladivostok Developer Conference #0");
+		response.end();
+	}).listen(8888);
+	
+	console.log("Nodejs server started");
+};
+
+exports.start = start;
