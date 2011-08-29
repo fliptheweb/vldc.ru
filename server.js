9d68de478dfc884da47a23e2e92c5e4108202eff
fliptheweb <itisshortemailaddress@gmail.com>
2011-08-29 19:15:45 +1100

    Add url parse

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 var http = require("http");
+var url = require("url");
 
 /**
  * Start out http server
  */
 function start(){
-	http.createServer(function(request, response) {
+	function onRequest(request, response) {
+		var pathname = url.parse(request.url).pathname;
+		console.log("Request for " + pathname + " received.");
 		response.writeHead(200, {"Content-Type": "text/html"});
 		response.write("Vladivostok Developer Conference #0");
 		response.end();
-	}).listen(8888);
-	
+	}
+
+	http.createServer(onRequest).listen(8888);
 	console.log("Nodejs server started");
 };
 
