72b06494da84e33a448a253224764d479b9eb5e0
fliptheweb <itisshortemailaddress@gmail.com>
2011-09-22 23:59:01 +1100

    Connect jade templates

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 var app = require('express').createServer();
 
-app.get('/', function(req, res){
-	res.send('VLDC #1');
+//configurate our server
+app.configure(function(){
+    app.set('view engine', 'jade');             //we used jade templates
+    app.set('view options', {layout: false});   //dont use layout in `render` method
+});
+
+//all path routed to root
+app.get('/*', function(req, res){
+	res.render('index');
 });
 
 app.listen(3000);
