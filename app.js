c953f32779abf9864caf2d0f546b945f1ad507bc
fliptheweb <itisshortemailaddress@gmail.com>
2011-09-23 00:39:32 +1100

    Send layout static files and rewire index template

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ app.configure(function(){
     app.set('view options', {layout: false});   //dont use layout in `render` method
 });
 
+//output static files (i think proxy nginx better :)
+app.get("/layout/*", function(req, res){
+    res.download('layout/'+req.params);
+});
+
 //all path routed to root
 app.get('/*', function(req, res){
 	res.render('index');
