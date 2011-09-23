d9b45fd540eb93ae380c616394d0c0ccf80b5270
fliptheweb <itisshortemailaddress@gmail.com>
2011-09-23 21:44:39 +1100

    Add 404 error page

--- app.js
+++ app.js
@@ -6,13 +6,27 @@ app.configure(function(){
     app.set('view options', {layout: false});   //dont use layout in `render` method
 });
 
-//output static files (i think proxy nginx better :)
-app.get("/layout/*", function(req, res){
-    res.download('layout/'+req.params);
+app.use(app.router);
+//if no one routes don't match
+app.use(function(req, res){
+    //TODO create jade template for 404 page
+    res.send('Извините, страница не найдена', 404);
+});
+
+//Routes
+
+//output static files
+app.get("/layout/*", function(req, res, next){
+    res.sendfile('layout/'+req.params, function(err){
+        //go to next suitable route if error
+        if(err){
+            next();
+        }
+    });
 });
 
 //all path routed to root
-app.get('/*', function(req, res){
+app.get('/', function(req, res){
 	res.render('index');
 });
 
