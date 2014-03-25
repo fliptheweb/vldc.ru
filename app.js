deffca6a0dad915eefefc526403d7ebf06290abf
Artur Kornakov <itisshortemailaddress@gmail.com>
2014-03-25 15:17:19 +1100

    Remove all db connctions

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 var
-//  db = require('./db'),
   express = require('express'),
   app = express(),
   DEFAULT_PORT = 12000,
-  PORT = DEFAULT_PORT,
+  PORT = process.env.PORT || process.argv[2] || DEFAULT_PORT,
   title = "VLDC — Владивостокская конференция разработчиков";
 
 app.set('view engine', 'jade');
@@ -27,85 +26,6 @@ app.get('/', function(req, res) {
   });
 });
 
-app.post('/', function(req, res) {
-  var registered = req.cookies.registered || 0;
-  console.log(req.headers);
-  console.log(req.body);
-  console.log(req.query);
-  var request = req.body,
-    surname = request.surname || "",
-    name = request.name || "",
-    email = request.email || "",
-    workplace = request.workplace || "",
-    emailRex = /^[-a-z0-9!#$%&'*+/=?^_`{|}~]+(?:\.[-a-z0-9!#$%&'*+/=?^_`{|}~]+)*@(?:[a-z0-9]([-a-z0-9]{0,61}[a-z0-9])?\.)*(?:aero|arpa|asia|biz|cat|com|coop|edu|gov|info|int|jobs|mil|mobi|museum|name|net|org|pro|tel|travel|[a-z][a-z])$/,
-    emailOk = email.match(emailRex),
-    sanitizer = require('sanitizer');
-
-    if (!surname || !name || !email || !workplace || !emailOk) {
-    res.render('index', {
-      title: title,
-      flash: "Заполните, пожалуйста, все поля",
-      error: true,
-      name: name,
-      surname: surname,
-      email: email,
-      workplace: workplace,
-      registered: registered
-    });
-  }
-
-  name = surname + ' ' + name;
-  name = sanitizer.escape(sanitizer.sanitize(name));
-  email = sanitizer.escape(sanitizer.sanitize(email));
-  workplace = sanitizer.escape(sanitizer.sanitize(workplace));
- 
-  var query = 'select count(id) cnt from event_member where ' +
-    'event_id=1 and name = \'' + name + '\'';
-
-  db.client.query(query, function(err, result) {
-    if (err) {
-      console.error('error running query', err);
-      res.json({error: err});
-    }
-    var isExist = result.rows[0].cnt > 0;
-    if (isExist) {
-      res.render('index',
-        {
-          title: title,
-          flash: "Извините, но вы уже зарегистрированы",
-          error: true,
-          name: name,
-          surname: surname,
-          email: email,
-          workplace: workplace,
-          registered: registered
-        });
-    } else {
-      query = 'insert into event_member (event_id, name, date, email, workplace)' +
-        'values (1, \''+name+'\', now(), \''+email+'\', \''+workplace+'\')';
-      db.client.query(query, function(err, result) {
-        if (err) {
-          res.json(err)
-        } else {
-          var MAGICK_NUMBER = 1000000000;
-          res.cookie('registered', '1', {expires: new Date(Date.now() + MAGICK_NUMBER)});
-          //todo send email
-          res.render('index',
-            {
-              title: title,
-              flash: "Все отлично, ждем вас на конференции!",
-              error: false,
-              registered: 1
-            });
-        }
-      });
-    }
-
-  });
+app.listen(PORT, function() {
+	console.log('Running on http://127.0.0.1:' + PORT + '/');
 });
-
-//db.connectDatabase(function() {
-//  app.listen(PORT, function() {
-//    console.log('Running on http://127.0.0.1:' + PORT + '/');
-//  });
-//});
